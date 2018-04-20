511617d87a37c1e5a04881c148cfa0dcaf6bd555
oliverjensen <oliver.damsgaard@gmail.com>
2018-04-20 20:35:28 +0200

    add key_dictionary

--- record_keyboard.py
+++ record_keyboard.py
@@ -1,29 +1,27 @@
 
 
-#from msvcrt import getch
-#while True:
-#    print(ord(getch()))
-
-
 from pynput import keyboard
+class KeyBoardListner:
+    def __init__(self):
+        self.key_dict = {'w':False, 's': False, 'a':False, 'd':False}
+        self.run()
 
-def on_press(key):
-    try:
-    	print(key.char, "press")
-    	return key.char, 1
-    except AttributeError:
-        pass
+    def on_press(key):
+        try:
+        	if key in self.key_dict:
+        	       self.key_dict[key] = True
+        except AttributeError:
+            pass
 
-def on_release(key):
-   	
-    if key == keyboard.Key.esc:
-        return False# Stop listener
-    else:
-    	print(key.char, "release")
-    	return key, -1
+    def on_release(key):
+        if key == keyboard.Key.esc:
+            return False# Stop listener
+        else:
+            if key in self.key_dict:
+                self.key_dict[key] = False
 
-# Collect events until released
-with keyboard.Listener(
-        on_press=on_press,
-        on_release=on_release) as listener:
-    listener.join()
+    def run(self):
+        with keyboard.Listener(
+                on_press=on_press,
+                on_release=on_release) as listener:
+            listener.join()
